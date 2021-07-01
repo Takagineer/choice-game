c12ad8144310fdf6ec94e2f359f0a0d17f6b5df1
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-07-01 13:17:39 +0900

    create-shuffle-array

--- main.js
+++ main.js
@@ -7,8 +7,28 @@
 
   const quizSet = [
     { q: "what is A?", c: ["a", "b", "c"] },
-    { q: "what is B?", c: ["a", "b", "c"] },
-    { q: "what is C?", c: ["a", "b", "c"] },
+    { q: "what is B?", c: ["d", "e", "f"] },
+    { q: "what is C?", c: ["g", "h", "i"] },
   ];
   let currentNum = 0;
+
+  question.textContent = quizSet[currentNum].q;
+
+  function shuffle(arr) {
+    //上の記述で配列の最終indexをiという変数で定義している。
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
+
+    return arr;
+  }
+
+  const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+
+  shuffledChoices.forEach((choice) => {
+    const li = document.createElement("li");
+    li.textContent = choice;
+    choices.appendChild(li);
+  });
 }
