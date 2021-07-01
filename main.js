19a8444270ddcfbf43fcfdbad4c1fa480eaba835
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-07-01 15:17:57 +0900

    complete

--- main.js
+++ main.js
@@ -4,13 +4,18 @@
   const question = document.getElementById("question");
   const choices = document.getElementById("choices");
   const btn = document.getElementById("btn");
+  const result = document.getElementById("result");
+  const scoreLabel = document.querySelector("#result > p");
 
-  const quizSet = [
-    { q: "what is A?", c: ["a", "b", "c"] },
-    { q: "what is B?", c: ["d", "e", "f"] },
-    { q: "what is C?", c: ["g", "h", "i"] },
-  ];
+  const quizSet = shuffle([
+    { q: "世界一高い山は?", c: ["富士山", "力道山", "石鎚山", "ヒマラヤ山脈"] },
+    { q: "以下のうち、本物は?", c: ["長州力", "長州小力", "消臭力", "超非力"] },
+    { q: "どれが正解?", c: ["g", "h", "i", "j"] },
+  ]);
+
   let currentNum = 0;
+  let isAnswered;
+  let score = 0;
 
   function shuffle(arr) {
     //上の記述で配列の最終indexをiという変数で定義している。
@@ -23,15 +28,28 @@
   }
 
   function checkAnswer(li) {
+    if (isAnswered) {
+      return;
+    }
+    isAnswered = true;
+
     if (li.textContent === quizSet[currentNum].c[0]) {
       li.classList.add("correct");
+      score++;
     } else {
       li.classList.add("wrong");
     }
+    btn.classList.remove("disabled");
   }
 
   function setQuiz() {
+    isAnswered = false;
+
     question.textContent = quizSet[currentNum].q;
+
+    while (choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
 
     const shuffledChoices = shuffle([...quizSet[currentNum].c]);
     shuffledChoices.forEach((choice) => {
@@ -43,7 +61,25 @@
       });
       choices.appendChild(li);
     });
+    if (currentNum === quizSet.length - 1) {
+      btn.textContent = "show score";
+    }
   }
 
   setQuiz();
+
+  btn.addEventListener("click", () => {
+    if (btn.classList.contains("disabled")) {
+      return;
+    }
+
+    if (currentNum === quizSet.length - 1) {
+      scoreLabel.textContent = `score:${score}/${quizSet.length}`;
+      result.classList.remove("hidden");
+    } else {
+      btn.classList.add("disabled");
+      currentNum++;
+      setQuiz();
+    }
+  });
 }
