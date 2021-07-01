4acd1f7c28e000320a2ea91b2472b85aabfe8c8f
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-07-01 11:21:38 +0900

    quiz-data

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 "use strict";
 
 {
+  const question = document.getElementById("question");
+  const choices = document.getElementById("choices");
+  const btn = document.getElementById("btn");
+
+  const quizSet = [
+    { q: "what is A?", c: ["a", "b", "c"] },
+    { q: "what is B?", c: ["a", "b", "c"] },
+    { q: "what is C?", c: ["a", "b", "c"] },
+  ];
+  let currentNum = 0;
 }
